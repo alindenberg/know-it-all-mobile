17944af81a13d4fd92c6ba2fd71495851ebd7497
Austin Lindenberg <alindenberg8@gmail.com>
2019-06-18 08:50:20 -0700

    indentation fix

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,15 +14,15 @@ export default class LoginScreen extends React.Component {
       return (
         <View style={{ flex: 1, padding: 10, alignItems: "center" }}>
             <TextInput
-                style={{height: 40}}
-                placeholder="Username"
-                onChangeText={(text) => this.setState({text})}
+              style={{height: 40}}
+              placeholder="Username"
+              onChangeText={(text) => this.setState({text})}
             />
             <TextInput
-                style={{height: 40}}
-                placeholder="Password"
-                secureTextEntry={true}
-                onChangeText={(text) => this.setState({text})}
+              style={{height: 40}}
+              placeholder="Password"
+              secureTextEntry={true}
+              onChangeText={(text) => this.setState({text})}
             />
           <Button title="Login" onPress={this._signInAsync} />
           <Button style={{padding: 10}} title="Sign Up" onPress={() => this.props.navigation.navigate('SignUp')} />
